61bbba7765bca26e6ed5fad5a711c35f76db5453
Carsten <carsten.langer@gmx.de>
2024-02-12 22:01:59 +0100

    moves Main.scala to package hcd

--- src/main/scala/hcd/Main.scala
+++ src/main/scala/hcd/Main.scala
@@ -1,3 +1,5 @@
+package hcd
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("There is no main program yet. Please run the Scala tests, which include a test for the distribution!")
