4d45bc7c834a3c1362d651c76e375455f627f460
Carsten <carsten.langer@gmx.de>
2024-02-24 23:26:16 +0100

    fixes bug which would have thrown if the assigned topic was not part of the selected topic (a situation which would not have occurred in the fullcombinatoric algorithm)

--- src/main/scala/hcd/model/Metric.scala
+++ src/main/scala/hcd/model/Metric.scala
@@ -19,7 +19,7 @@ object Metric {
           val studentMetrics = studentAssignments.map { case (studentId, workshopIds) =>
             val assignedTopicsIds = workshopIds.map(workshops).toList.map { case (topicId, _, _, _) => topicId } // .toList is redundant to business logic
             val (_, selectedTopics) = studentsSelectedTopics(studentId)
-            val selectionPriorities = assignedTopicsIds.map(selectedTopics)
+            val selectionPriorities = assignedTopicsIds.map(selectedTopics.get).map(_.getOrElse(SelectionPriority(7)))
             val categories = assignedTopicsIds.map(topics)
             val metricPrios = metricFromSelectionPriorities(selectionPriorities)
             val metricCategories = metricFromCategories(categories)
