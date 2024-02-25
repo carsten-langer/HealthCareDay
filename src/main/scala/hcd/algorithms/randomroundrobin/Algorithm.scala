28279593fe62f6bf70540d9e1e4e300546d97e1d
Carsten Langer <carsten.langer@ista.com>
2024-02-25 22:16:50 +0100

    switches algorithm to recursion, early stage, still with hard-coded result

--- src/main/scala/hcd/algorithms/randomroundrobin/Algorithm.scala
+++ src/main/scala/hcd/algorithms/randomroundrobin/Algorithm.scala
@@ -6,7 +6,23 @@ import hcd.model._
 object Algorithm extends StrictLogging {
 
   /** This algorithm's distribution function. */
-  def distributionAlgorithm: DistributionAlgorithm = (_: Topics, _: Workshops) => (_: StudentsSelectedTopics) =>
-    Some(Map.empty)
+  def distributionAlgorithm: DistributionAlgorithm =
+    (_: Topics, _: Workshops) => (_: StudentsSelectedTopics) => {
+
+      final case class Student()
+
+      def recursion(workshopAssignments: WorkshopAssignments, remainingStudents: List[Student]): Option[WorkshopAssignments] =
+        remainingStudents match {
+          case Nil =>
+            logger.debug("end of recursion")
+            Some(workshopAssignments)
+          case _ => ???
+        }
+
+      val maybeWorkshopAssignments = recursion(workshopAssignments = Map.empty, remainingStudents = List.empty)
+      logger.debug(s"maybeWorkshopAssignments: $maybeWorkshopAssignments")
+      maybeWorkshopAssignments
+
+    }
 
 }
