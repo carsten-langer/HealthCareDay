40eeabbc6f7ae7a8cd00a4abede12cb30428093c
Carsten Langer <carsten.langer@ista.com>
2024-02-26 20:29:10 +0100

    adds test with a single student not having any selection. This triggers that a non-empty studentsSelectedTopics is considered in general in the algorithm. Still with partly hard-coded result.

--- src/main/scala/hcd/algorithms/randomroundrobin/Algorithm.scala
+++ src/main/scala/hcd/algorithms/randomroundrobin/Algorithm.scala
@@ -3,25 +3,32 @@ package hcd.algorithms.randomroundrobin
 import com.typesafe.scalalogging.StrictLogging
 import hcd.model._
 
+import scala.annotation.tailrec
+
 object Algorithm extends StrictLogging {
 
   /** This algorithm's distribution function. */
   def distributionAlgorithm: DistributionAlgorithm =
-    (_: Topics, workshops: Workshops) => (_: StudentsSelectedTopics) => {
+    (_: Topics, workshops: Workshops) => (studentsSelectedTopics: StudentsSelectedTopics) => {
 
       final case class Student()
 
+      val students = studentsSelectedTopics.toList.map(_ => Student())
+
+      @tailrec
       def recursion(workshopAssignments: WorkshopAssignments, remainingStudents: List[Student]): Option[WorkshopAssignments] =
         remainingStudents match {
           case Nil =>
             logger.debug("end of recursion")
             Some(workshopAssignments)
-          case _ => ???
+          case _ =>
+            val updatedWorkshopAssignments = workshopAssignments.updated(WorkshopId(0), Set(StudentId(1)))
+            recursion(updatedWorkshopAssignments, List.empty)
         }
 
       val emptyWorkshopAssignments = workshops.view.mapValues(_ => Set.empty[StudentId]).toMap
 
-      val maybeWorkshopAssignments = recursion(emptyWorkshopAssignments, remainingStudents = List.empty)
+      val maybeWorkshopAssignments = recursion(emptyWorkshopAssignments, remainingStudents = students)
       logger.debug(s"maybeWorkshopAssignments: $maybeWorkshopAssignments")
       maybeWorkshopAssignments
 
--- src/test/scala/hcd/algorithms/randomroundrobin/AlgorithmSpec.scala
+++ src/test/scala/hcd/algorithms/randomroundrobin/AlgorithmSpec.scala
@@ -2,6 +2,8 @@ package hcd.algorithms.randomroundrobin
 
 import hcd.algorithms.fixtureSymmetricWorkshopsFor
 import hcd.algorithms.randomroundrobin.Algorithm.distributionAlgorithm
+import hcd.model.{SelectionPriority, StudentId, TopicId, WorkshopId}
+import io.cvbio.collection.mutable.bimap.BiMap
 import org.scalatest.OptionValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -29,6 +31,18 @@ class AlgorithmSpec
 
         distributionAlgorithm(Map.empty, f.workshops)(Map.empty).value shouldEqual expectedWorkshopAssignments
         distributionAlgorithm(f.topics, f.workshops)(Map.empty).value shouldEqual expectedWorkshopAssignments
+      }
+
+      "assigns a workshop to a student which has not selected any topic" in {
+        val f = fixtureSymmetricWorkshopsFor(noTopics = 1)
+        val student1 = StudentId(1)
+        val studentsSelectedTopics = Map(student1 -> (f.grade, BiMap.empty[TopicId, SelectionPriority]))
+        // student1 has no selection, thus gets assigned next best workshop
+        val expectedWorkshopAssignments = Map(
+          WorkshopId(0) -> Set(student1), WorkshopId(1) -> Set.empty, WorkshopId(2) -> Set.empty, // TopicId(0)
+        )
+
+        distributionAlgorithm(f.topics, f.workshops)(studentsSelectedTopics).value shouldEqual expectedWorkshopAssignments
       }
 
     }
