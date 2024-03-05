c878eaed79236f6e91b76e9991f3c67b2c6c7750
Carsten <carsten.langer@gmx.de>
2024-03-05 21:59:10 +0100

    refactors to switch from metricFromSelectionPriorities() to metricFromSelectionPriority()

--- src/main/scala/hcd/model/Metric.scala
+++ src/main/scala/hcd/model/Metric.scala
@@ -16,13 +16,13 @@ object Metric {
   def metric(topics: Topics, workshops: Workshops, studentsSelectedTopics: StudentsSelectedTopics)(workshopAssignments: WorkshopAssignments): Metric = {
     val studentAssignments = studentAssignmentsFrom(workshopAssignments)
     val studentMetrics = studentAssignments.map { case (studentId, workshopIds) =>
-      val assignedTopicsIds = workshopIds.map(workshops).toList.map { case (topicId, _, _, _) => topicId } // .toList is redundant to business logic
+      val assignedTopicIds = workshopIds.map(workshops).toList.map { case (topicId, _, _, _) => topicId } // .toList is redundant to business logic
       val (_, selectedTopics) = studentsSelectedTopics(studentId)
-      val selectionPriorities = assignedTopicsIds.map(selectedTopics.get).map(_.getOrElse(SelectionPriority(7)))
-      val categories = assignedTopicsIds.map(topics)
-      val metricPrios = metricFromSelectionPriorities(selectionPriorities)
+      val selectionPriorities = assignedTopicIds.map(selectedTopics.get).map(_.getOrElse(SelectionPriority(7)))
+      val categories = assignedTopicIds.map(topics)
+      val metricsPrios = selectionPriorities.map(metricFromSelectionPriority)
       val metricCategories = metricFromCategories(categories)
-      studentId -> add(metricPrios, metricCategories)
+      studentId -> add(metricCategories, metricsPrios)
     }
     studentMetrics.values.toList match {
       case ::(head, next) => add(head, next)
@@ -30,8 +30,8 @@ object Metric {
     }
   }
 
-  /** Simple linear metric on priorities. */
-  def metricFromSelectionPriorities(prios: Iterable[SelectionPriority]): Metric = Metric(prios.map(_.prio).sum)
+  /** Simple linear metric from selection priority. */
+  def metricFromSelectionPriority(selectionPriority: SelectionPriority): Metric = Metric(selectionPriority.prio)
 
   /** Malus if a combo contains only sports category. */
   def metricFromCategories(categories: Iterable[Category]): Metric =
