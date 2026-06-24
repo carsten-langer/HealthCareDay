b0188734a43743bf55be658f545e06379f9a2e01
Carsten <carsten.langer@gmx.de>
2026-06-24 13:12:32 +0200

    Revert "limits to 1 timeslot with minimal changes of the source code"

--- src/main/scala/hcd/algorithms/randomroundrobin/Algorithm.scala
+++ src/main/scala/hcd/algorithms/randomroundrobin/Algorithm.scala
@@ -3,7 +3,6 @@ package hcd.algorithms.randomroundrobin
 import com.typesafe.scalalogging.StrictLogging
 import hcd.model.Metric.metricGlobal
 import hcd.model.SelectionPriority.worstPrio
-import hcd.model.TimeSlot.FirstTimeSlot
 import hcd.model._
 
 import java.time.LocalTime
@@ -57,7 +56,7 @@ object Algorithm extends StrictLogging {
             studentId = studentId,
             grade = grade,
             topicSelections = orderedTopicSelection,
-            unassignedTimeSlots = Set(FirstTimeSlot),
+            unassignedTimeSlots = allTimeSlots,
             assignedTopics = Set.empty)
       }.sortBy(_.sortingOrder)
 
